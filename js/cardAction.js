c786bc4b4db1ffbcf7c0a78fd17674ab41ea132a
Dzjanis <Roah.thaar.g@gmail.com>
2025-04-24 08:35:52 +0300

    add card flip logic

--- js/cardAction.js
+++ js/cardAction.js
@@ -6,16 +6,17 @@ console.log('hello from cardAction');
 const playground = document.querySelector('.playground');
 let cardsOpenCounter = 0;
 
-playground.addEventListener('click', (event) => {
+playground.addEventListener('click', function (event) {
   console.log('pin' + event.target);
   const card = event.target.closest('.flip__card');
-  if (card) {
+  if (card && !card.classList.contains('active')) {
     cardsOpenCounter++;
-    console.log('pin ' + cardsOpenCounter + card);
-    if (cardsOpenCounter < 2) {
+    console.log('pin ' + cardsOpenCounter + card.classList);
+    if (cardsOpenCounter <= 2) {
       card.classList.add('active');
       console.log('pin');
-    } else {
+    }
+    if (cardsOpenCounter === 2) {
       setTimeout(() => {
         document.querySelectorAll('.flip__card').forEach(card => {
           card.classList.remove('active');
